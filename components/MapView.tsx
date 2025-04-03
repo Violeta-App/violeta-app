0dffdc3fca7547424f6a81dca025c5263a08a76b
Jvcregis <jvcr@cin.ufpe.br>
2025-04-03 17:36:55 -0300

    feat/routes: coloring routes feature implemented

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { StyleSheet, View, Dimensions, Image, Alert, Text, Modal, TouchableOpacity, TextInput } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Region, Marker, Callout } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Region, Marker, Polyline } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import Geocoder from 'react-native-geocoding';
 import { PermissionsAndroid } from 'react-native';
@@ -31,35 +31,51 @@ const GOOGLE_MAPS_APIKEY = 'AIzaSyDJcZ1QMu2IpPHzNDarAfLrTRrtrBHH3_8';
 
 const MapScreen: React.FC = () => {
   const mapRef = useRef<MapView>(null);
+
+  // Alert
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedAlert, setSelectedAlert] = useState<AlertType | null>(null);
+
+  // MapView
   const [origin, setOrigin] = useState({ latitude: 0, longitude: 0 });
   const [destination, setDestination] = useState({ latitude: 0, longitude: 0 });
+
+  // Routes
   const [originText, setOriginText] = useState('');
   const [destinationText, setDestinationText] = useState('');
+  const [routeCoordinates, setRouteCoordinates] = useState<any[]>([]);
+  const [isRouteRequested, setIsRouteRequested] = useState(false);
+  const [segmentsColors, setSegmentsColors] = useState<string[]>([]);
 
+  // Requesting location permission
   useEffect(() => {
-    // Solicitar permissões de localização no Android
-    const requestLocationPermission = async () => {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Permission to access location',
-          message: 'We need your location to show directions',
-          buttonPositive: 'OK',  // Adicione isso
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        getLocation();
+    const checkAndRequestPermission = async () => {
+      const hasPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      
+      if (hasPermission) {
+        getUserLocation();
       } else {
-        Alert.alert('Location permission denied');
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Permission to access location',
+            message: 'We need your location to show directions',
+            buttonPositive: 'OK',
+          }
+        );
+  
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getUserLocation();
+        } else {
+          Alert.alert('Location permission denied');
+        }
       }
-    };    
-
-    requestLocationPermission();
+    };
+  
+    checkAndRequestPermission();
   }, []);
 
-  const getLocation = () => {
+  const getUserLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
@@ -85,6 +101,7 @@ const MapScreen: React.FC = () => {
     setIsModalVisible(false);
   };
 
+  // Route calculation
   const handleSearch = () => {
     if (originText) {
       Geocoder.init(GOOGLE_MAPS_APIKEY);
@@ -109,6 +126,25 @@ const MapScreen: React.FC = () => {
     } else {
       alert('Please enter a destination address');
     }
+
+    if (originText && destinationText) {
+      setIsRouteRequested(true)
+    }
+  };
+
+  const getSegmentColor = (): string => {
+    const colors = ['#CF5C36', '#04724D', '#FFD936'];
+
+    // Aqui implementaremos a lógica de obter a cor via métrica de segurança. No momento isso está sendo feito aleatoriamente.
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  const handleRouteReady = (result: any) => {
+    setRouteCoordinates(result.coordinates); // Retorna as coordenadas que formam a rota gerada automaticamente pelo MapViewDirections
+
+    const colors = result.coordinates.map(() => getSegmentColor()); // Mapeia cada subsegmento da rota para uma cor
+    setSegmentsColors(colors);
   };
 
   return (
@@ -128,18 +164,48 @@ const MapScreen: React.FC = () => {
           </Marker>
         ))}
 
-        {origin.latitude !== 0 && destination.latitude !== 0 && (
+        {/* Calcula a rota entre dois pontos automaticamente */}
+        {isRouteRequested && origin.latitude !== 0 && destination.latitude !== 0 && (
           <MapViewDirections
             origin={origin}
             destination={destination}
             apikey={GOOGLE_MAPS_APIKEY}
-            strokeWidth={4}
-            strokeColor="blue"
+            strokeWidth={0} // Para não renderizar a rota
+            splitWaypoints={true}
+            precision="low"
+            onStart={(params) => {
+              console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+            }}
+            onReady={handleRouteReady}
+            onError={(errorMessage) => {
+              console.log('Error: ', errorMessage);
+            }}
           />
         )}
+
+        {/* Renderiza a rota gerada acima com cores diferentes para cada subsegmento */}
+        {isRouteRequested && routeCoordinates.length > 0 &&
+          routeCoordinates.map((_, index) => {
+            if (index < routeCoordinates.length - 1) {
+              const segment = [
+                routeCoordinates[index], 
+                routeCoordinates[index + 1]
+              ];
+              return (
+                <Polyline
+                  key={`segment-${index}`}
+                  coordinates={segment}
+                  strokeColor={segmentsColors[index]}
+                  strokeWidth={4}
+                />
+              );
+            }
+            return null;
+          })
+        }
       </MapView>
 
-      {/* Modal */}
+      {/* Alert Modal */}
       {selectedAlert && (
         <Modal
           animationType="slide"
